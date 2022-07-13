abc0a3fd12a88f8d4d26c34099e23c82de2bb650
Anna Podobrii <anna@nearmeentertainment.com>
2022-07-13 18:47:37 +0300

    updated detail info about weather

--- scr/script.js
+++ scr/script.js
@@ -1,6 +1,9 @@
 // search Form
 var city = document.querySelector("#city-name");
 var searchPlace = document.querySelector("#exampleDataList");
+let apiKey = "8fa673d34c240a0cb12974c973229b0e";
+var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+ 
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -13,63 +16,55 @@ function searchFormCity(event) {
     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
   }
   const searchPlaceGoorView = arr.join(" ");
-  city.innerHTML = searchPlaceGoorView;
-  let apiKey = "8fa673d34c240a0cb12974c973229b0e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.innerHTML}&appid=${apiKey}&units=metric`;
-  function weather(response) {
-    let currentTemperature = Math.round(response.data.main.temp);
-    let weatherSentens = document.querySelector("#temp");
-    let weatherIcon = document.querySelector("#weatherIcon");
-    let weatherName = document.querySelector("#name-weather");
-    let weatherDescription = document.querySelector("#description-weather");
-
-    weatherSentens.innerHTML = `${currentTemperature}`;
-    weatherIcon.setAttribute(
-      "src",
-      `images/${response.data.weather[0].icon}.png`
-    );
-    weatherName.innerHTML = `${response.data.weather[0].main}`;
-    weatherDescription.innerHTML = capitalizeFirstLetter(
-      `${response.data.weather[0].description}`
-    );
-    console.log(response);
-  }
-  axios.get(apiUrl).then(weather);
+  city.innerHTML = searchPlaceGoorView
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.innerHTML}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weatherCurrent);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchFormCity);
 
 function currentPosition(position) {
-  let apiKey = "8fa673d34c240a0cb12974c973229b0e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  function weatherCurrent(response) {
-    let currentTemperature = Math.round(response.data.main.temp);
-    let weatherSentens = document.querySelector("#temp");
-    let weatherIcon = document.querySelector("#weatherIcon");
-    let weatherName = document.querySelector("#name-weather");
-    let weatherDescription = document.querySelector("#description-weather");
-
-    weatherSentens.innerHTML = `${currentTemperature}`;
-    city.innerHTML = `${response.data.name}`;
-    weatherIcon.setAttribute(
-      "src",
-      `images/${response.data.weather[0].icon}.png`
-    );
-    weatherName.innerHTML = `${response.data.weather[0].main}`;
-    weatherDescription.innerHTML = capitalizeFirstLetter(
-      `${response.data.weather[0].description}`
-    );
-  }
-
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(weatherCurrent);
 }
 function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(currentPosition);
 }
-
 let searchCurrent = document.querySelector("#local-button");
 searchCurrent.addEventListener("click", getCurrentLocation);
 
+function weatherCurrent(response) {
+  let currentTemperature = Math.round(response.data.main.temp);
+  let weatherSentens = document.querySelector("#temp");
+  let weatherIcon = document.querySelector("#weatherIcon");
+  let weatherName = document.querySelector("#name-weather");
+  let weatherDescription = document.querySelector("#description-weather");
+  let maxTemp = document.querySelector("#high");
+  let minTemp = document.querySelector("#low");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let presure = document.querySelector("#presure");
+  let minTemperature = Math.round(response.data.main.temp_min);
+  let maxTemperature = Math.round(response.data.main.temp_max);
+
+  weatherSentens.innerHTML = `${currentTemperature}`;
+  city.innerHTML = `${response.data.name}`;
+  weatherIcon.setAttribute(
+    "src",
+    `images/${response.data.weather[0].icon}.png`
+  );
+  weatherName.innerHTML = `${response.data.weather[0].main}`;
+  weatherDescription.innerHTML = capitalizeFirstLetter(
+    `${response.data.weather[0].description}`
+  );
+  minTemp.innerHTML = `${minTemperature}`;
+  maxTemp.innerHTML = `${maxTemperature}`;
+  humidity.innerHTML = `${response.data.main.humidity}`;
+  wind.innerHTML = `${response.data.wind.speed}`;
+  presure.innerHTML = `${response.data.main.pressure}`;
+  console.log(response)
+}
+axios.get(apiUrl).then(weatherCurrent);
 // Date
 function formatDate(date) {
   let days = [
