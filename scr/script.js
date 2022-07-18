95bc18c513223888d9b5e58ee0ccf85c4174272b
Anna Podobrii <anna@nearmeentertainment.com>
2022-07-18 11:46:52 +0300

    last commit

--- scr/script.js
+++ scr/script.js
@@ -194,35 +194,6 @@ let searchCurrent = document.querySelector("#local-button");
 searchCurrent.addEventListener("click", getCurrentLocation);
 
 function weatherCurrent(response) {
-  celsiusTemp = response.data.main.temp;
-  // let currentTemperature = Math.round(response.data.main.temp);
-  // let weatherSentens = document.querySelector("#temp");
-  // let weatherIcon = document.querySelector("#weatherIcon");
-  // let weatherName = document.querySelector("#name-weather");
-  // let weatherDescription = document.querySelector("#description-weather");
-  // let maxTemp = document.querySelector("#high");
-  // let minTemp = document.querySelector("#low");
-  // let humidity = document.querySelector("#humidity");
-  // let wind = document.querySelector("#wind");
-  // let presure = document.querySelector("#presure");
-  // let minTemperature = Math.round(response.data.main.temp_min);
-  // let maxTemperature = Math.round(response.data.main.temp_max);
-
-  // weatherSentens.innerHTML = `${currentTemperature}`;
-  // city.innerHTML = `${response.data.name}`;
-  // weatherIcon.setAttribute(
-  //   "src",
-  //   `images/${response.data.weather[0].icon}.png`
-  // );
-  // weatherName.innerHTML = `${response.data.weather[0].main}`;
-  // weatherDescription.innerHTML = capitalizeFirstLetter(
-  //   `${response.data.weather[0].description}`
-  // );
-  // minTemp.innerHTML = `${minTemperature}`;
-  // maxTemp.innerHTML = `${maxTemperature}`;
-  // humidity.innerHTML = `${response.data.main.humidity}`;
-  // wind.innerHTML = `${response.data.wind.speed}`;
-  // presure.innerHTML = `${response.data.main.pressure}`;
   showDailyForecast(response.data.coord);
   showMainCurrentTemp(response.data.name);
 }
